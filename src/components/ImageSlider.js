f9e7f5d73622d9625ed5b78537e183efabf51a67
Your Name <malin.marie@hotmail.se>
2020-06-07 12:18:06 +0200

    smooth transition

--- src/components/ImageSlider.js
+++ src/components/ImageSlider.js
@@ -9,17 +9,17 @@ const responsive = {
   desktop: {
     breakpoint: { max: 3000, min: 1024 },
     items: 7,
-    slidesToSlide: 3 // optional, default to 1.
+    slidesToSlide: 5 
   },
   tablet: {
     breakpoint: { max: 1024, min: 464 },
     items: 3,
-    slidesToSlide: 2 // optional, default to 1.
+    slidesToSlide: 2 
   },
   mobile: {
     breakpoint: { max: 464, min: 0 },
     items: 1,
-    slidesToSlide: 1 // optional, default to 1.
+    slidesToSlide: 1 
   }
 };
 
@@ -42,29 +42,18 @@ export const ImageSlider = ({ fetchlink }) => {
     //återanvändbar carousel som använder props för att få in img src/ titel osv..
 
     <Carousel
-      additionalTransfrom={0}
-      // autoPlaySpeed={3000}
       swipeable={true}
       draggable={false}
       minimumTouchDrag={80}
       partialVisible
       arrows={true}
       slidesToSlide={3}
-      // showDots={true}
       responsive={responsive}
-      itemClass="carousel-wrapper"
-      ssr={true} // means to render carousel on server-side.
+      ssr={true}
       infinite={true}
-      // autoPlay={this.props.deviceType !== "mobile" ? true : false}
       autoPlaySpeed={1000}
       keyBoardControl={true}
-      customTransition="all .5"
-      transitionDuration={500}
-      containerClass="carousel-container"
-      removeArrowOnDeviceType={["tablet", "mobile"]}
-      // deviceType={this.props.deviceType}
-      dotListClass="custom-dot-list-style"
-      itemClass="carousel-item-padding-40-px"
+      customTransition="all 1.5s ease-in-out"
     >
       {movies.map(movie => {
         return (
