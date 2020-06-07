dff3d078c3b80fe102b46a5aba36f0d2974aaa4f
jenniferot <56264114+Jenniferot@users.noreply.github.com>
2020-06-07 19:49:55 +0200

    testing button

--- src/components/ImageSlider.js
+++ src/components/ImageSlider.js
@@ -6,17 +6,17 @@ const responsive = {
   desktop: {
     breakpoint: { max: 3000, min: 1024 },
     items: 7,
-    slidesToSlide: 5 
+    slidesToSlide: 5
   },
   tablet: {
     breakpoint: { max: 1024, min: 464 },
-    items: 3,
-    slidesToSlide: 2 
+    items: 4,
+    slidesToSlide: 2
   },
   mobile: {
     breakpoint: { max: 464, min: 0 },
     items: 1,
-    slidesToSlide: 1 
+    slidesToSlide: 1
   }
 }
 
--- src/components/ImageSliderAndTitle.js
+++ src/components/ImageSliderAndTitle.js
@@ -2,7 +2,7 @@ import React, { useEffect, useState } from 'react'
 import Carousel from 'react-multi-carousel';
 import 'react-multi-carousel/lib/styles.css';
 import { Link } from 'react-router-dom'
-
+import { ListButton } from '../components/ListButton'
 
 const API_KEY = process.env.REACT_APP_API_KEY
 
@@ -10,17 +10,17 @@ const responsive = {
   desktop: {
     breakpoint: { max: 3000, min: 1024 },
     items: 7,
-    slidesToSlide: 5 
+    slidesToSlide: 5
   },
   tablet: {
     breakpoint: { max: 1024, min: 464 },
-    items: 3,
-    slidesToSlide: 2 
+    items: 4,
+    slidesToSlide: 2
   },
   mobile: {
     breakpoint: { max: 464, min: 0 },
     items: 1,
-    slidesToSlide: 1 
+    slidesToSlide: 1
   }
 }
 
@@ -46,37 +46,39 @@ export const ImageSliderAndTitle = ({ fetchtitle, fetchlink }) => {
       <h4>{fetchtitle}</h4>
 
       <Carousel
-      swipeable={true}
-      draggable={false}
-      minimumTouchDrag={80}
-      partialVisible
-      arrows={true}
-      responsive={responsive}
-      ssr={true}
-      infinite={true}
-      autoPlaySpeed={1000}
-      keyBoardControl={true}
-      customTransition="all 1.5s ease-in-out"
-    >
-      {movies.map(movie => {
-        return (
-          <div className="movieWrapper" key={movie.id}>
-            <Link className="movieLink" to={`/movie/${movie.id}`}>
-              <img
-                className="movieImage"
-                draggable={false}
-                alt={movie.original_title}
-                style={{ width: "100%" }}
-                src={`https://image.tmdb.org/t/p/w342${movie.backdrop_path}`}
-              >
-              </img>
-              <div className="titleDate"> <h5>{movie.original_title}</h5> <p>Released {movie.release_date}</p></div>
-
-            </Link>
-          </div>
-        )
-      })}
-    </Carousel >
+        swipeable={true}
+        draggable={false}
+        minimumTouchDrag={80}
+        partialVisible
+        arrows={true}
+        responsive={responsive}
+        ssr={true}
+        infinite={true}
+        autoPlaySpeed={1000}
+        keyBoardControl={true}
+        customTransition="all 1.5s ease-in-out"
+      >
+        {movies.map(movie => {
+          return (
+            <div>
+              <Link to={`/movie/${movie.id}`}>
+                <div className="movieWrapper" key={movie.id}>
+                  <img
+                    className="movieImage"
+                    draggable={false}
+                    alt={movie.original_title}
+                    style={{ width: "100%" }}
+                    src={`https://image.tmdb.org/t/p/w342${movie.backdrop_path}`}
+                  >
+                  </img>
+                  <div className="titleDate"> <h5>{movie.original_title}</h5> <p>Released {movie.release_date}</p></div>
+                </div>
+              </Link>
+              <ListButton />
+            </div>
+          )
+        })}
+      </Carousel >
     </>
   )
 }
