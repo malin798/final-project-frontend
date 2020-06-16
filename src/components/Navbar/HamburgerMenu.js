859b7de9b2dd1b167ed42cfbb84418cfb3921f60
jenniferot <56264114+Jenniferot@users.noreply.github.com>
2020-06-16 02:43:01 +0200

    genres in hamburger

--- src/components/Navbar/HamburgerMenu.js
+++ src/components/Navbar/HamburgerMenu.js
@@ -17,7 +17,7 @@ export const HamburgerMenu = () => {
 
   return (
     <>
-      <div className={`hamburger-icon ${show ? "transform" : ""}`} 
+      <div className={`hamburger-icon ${show ? "transform" : ""}`}
         onClick={() => showMenu()}>
         <div className="hamburger-div-1"></div>
         <div className="hamburger-div-2"></div>
@@ -26,19 +26,38 @@ export const HamburgerMenu = () => {
       <ul className={`hamburger-menu ${show ? "visible" : ""}`}>
         <Link to="/"> <li>Home</li></Link>
         <Link to="/"> <li>Movies</li></Link>
-
-        <li onClick={() => handleClick()}>Genres</li>
-          <div className={`hamburger-menu-genre-accordion ${visible ? "visible" : ""}`}>
-            <Link to="/genres/Romance/10749">Romance</Link>
-          </div>
-
         <Link to="/top-rated" ><li>Top rated</li></Link>
         <Link to="/trending-today" ><li>Trending today</li></Link>
         <Link to="/trending-week"><li>Trending week</li></Link>
         <Link to="/upcoming" ><li>Upcoming</li></Link>
         <Link to="/now-playing"><li>Now playing</li></Link>
+        <li className="genres" onClick={() => handleClick()}>Genres<span><RightArrow /></span></li>
+        <div className={`hamburger-menu-genre-accordion ${visible ? "visible" : ""}`}><br />
+          <Link className="genre" to="/genres/Action/28">Action</Link><br />
+          <Link className="genre" to="/genres/Adventure/12">Adventure</Link><br />
+          <Link className="genre" to="/genres/Animation/16">Animation</Link><br />
+          <Link className="genre" to="/genres/Comedy/35">Comedy</Link><br />
+          <Link className="genre" to="/genres/Crime/80">Crime</Link><br />
+          <Link className="genre" to="/genres/Documentary/99">Documentary</Link><br />
+          <Link className="genre" to="/genres/Drama/18">Drama</Link><br />
+          <Link className="genre" to="/genres/Family/10751">Family</Link><br />
+          <Link className="genre" to="/genres/Fantasy/14">Fantasy</Link><br />
+          <Link className="genre" to="/genres/History/36">History</Link><br />
+          <Link className="genre" to="/genres/Horror/27">Horror</Link><br />
+          <Link className="genre" to="/genres/Music/10402">Music</Link><br />
+          <Link className="genre" to="/genres/Mystery/9648">Mystery</Link><br />
+          <Link className="genre" to="/genres/Romance/10749">Romance</Link><br />
+          <Link className="genre" to="/genres/Science%20Fiction/878">Science&nbsp;Fiction</Link><br />
+          <Link className="genre" to="/genres/Thriller/53">Thriller</Link><br />
+          <Link className="genre" to="/genres/War/10752">War</Link><br />
+          <Link className="genre" to="/genres/Western/37">Western</Link>
+        </div>
+
       </ul>
 
     </>
   )
 }
+
+
+//          <Link className="genre" to="/genres/Romance/10749">TV Movie</Link><br />
