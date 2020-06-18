4a7b367146e35594b77974fec41bc5c517ff721b
jenniferot <56264114+Jenniferot@users.noreply.github.com>
2020-06-18 14:12:22 +0200

    Co-authored-by: Malin Andersson <malin.marie@hotmail.se>

--- src/pages/Profile.js
+++ src/pages/Profile.js
@@ -28,15 +28,16 @@ export const Profile = ({ loggedIn, setLoggedIn }) => {
 
 
   const removeItem = (showId) => {
-    const showStringId = showId.toString();
+    //const showStringId = showId.toString();
     fetch(`http://localhost:8080/users/5ed91c84a1d0445020645add/watchlist`, {
       method: "DELETE",
       headers: {
+        "Content-Type": "application/json",
         "Authorization": "92cb03b6711b60551695b6ad33efd277918f03c4e92afdbf25b6df0297bcd34bb8f44c2c9e1ac8f39fef22af9d32b658b6a849990726b0b64090e241770a79f6b9e902dd987d9726488090e251f2d115979d01df574c1f6477668357255789ef56ea121727095e89f426c4b37d34cdfba91c9ff125ef72e94be8d29928470c9a"
       },
-      body: {
-        "showId": showStringId
-      }
+      body: JSON.stringify({
+        "showId": showId
+      })
     })
       .then(res => res.json())
       .then(json => {
@@ -45,8 +46,9 @@ export const Profile = ({ loggedIn, setLoggedIn }) => {
         // setList(watchlist)
         console.log(json)
       })
-    console.log(showStringId)
+    console.log(showId)
   }
+
 
   //let src = `https://image.tmdb.org/t/p/w342/${imageSrc}`
 
