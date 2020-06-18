db543ebf6f06b34af7c03bb579cb2bef6ce75bda
jenniferot <56264114+Jenniferot@users.noreply.github.com>
2020-06-18 14:22:36 +0200

    merged

--- src/pages/Profile.js
+++ src/pages/Profile.js
@@ -26,21 +26,12 @@ export const Profile = ({ loggedIn, setLoggedIn }) => {
 
 
   const removeItem = (showId) => {
-<<<<<<< HEAD
-    //const showStringId = showId.toString();
-    fetch(`http://localhost:8080/users/5ed91c84a1d0445020645add/watchlist`, {
-      method: "DELETE",
-      headers: {
-        "Content-Type": "application/json",
-        "Authorization": "92cb03b6711b60551695b6ad33efd277918f03c4e92afdbf25b6df0297bcd34bb8f44c2c9e1ac8f39fef22af9d32b658b6a849990726b0b64090e241770a79f6b9e902dd987d9726488090e251f2d115979d01df574c1f6477668357255789ef56ea121727095e89f426c4b37d34cdfba91c9ff125ef72e94be8d29928470c9a"
-=======
 
     fetch(`http://localhost:8080/users/${userId}/watchlist`, {
       method: "DELETE",
       headers: {
         "Content-Type": "application/json",
         "Authorization": `${accessToken}`
->>>>>>> c9bf4435414ad6dc59e4cfe9feec6093c28cfd04
       },
       body: JSON.stringify({
         "showId": showId
@@ -50,18 +41,8 @@ export const Profile = ({ loggedIn, setLoggedIn }) => {
       .then(json => {
         setList(json.watchlist)
       })
-<<<<<<< HEAD
-    console.log(showId)
   }
 
-
-  //let src = `https://image.tmdb.org/t/p/w342/${imageSrc}`
-
-
-=======
-  }
-
->>>>>>> c9bf4435414ad6dc59e4cfe9feec6093c28cfd04
   const capitalizeFirstLetter = (string) => {
     return string.charAt(0).toUpperCase() + string.slice(1);
   }
