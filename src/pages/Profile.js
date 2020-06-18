2014acd62a1070da19e4aebd59e29b0fe1da1a18
jenniferot <56264114+Jenniferot@users.noreply.github.com>
2020-06-18 13:46:18 +0200

    showId stringify

--- src/pages/Profile.js
+++ src/pages/Profile.js
@@ -8,6 +8,7 @@ export const Profile = ({ loggedIn, setLoggedIn }) => {
   const accessToken = useSelector((store) => store.user.login.accessToken)
   const userId = useSelector((store) => store.user.login.userId)
   const [list, setList] = useState([])
+  const userID = "5ed91c84a1d0445020645add"
 
   useEffect(() => {
     fetch("http://localhost:8080/users/5ed91c84a1d0445020645add/watchlist", {
@@ -21,10 +22,31 @@ export const Profile = ({ loggedIn, setLoggedIn }) => {
       .then(json => {
         setList(json.watchlist)
         //.then(watchlist => {
-        // console.log(watchlist)
+        console.log(json.watchlist)
       })
   }, [])
 
+
+  const removeItem = (showId) => {
+    const showStringId = showId.toString();
+    fetch(`http://localhost:8080/users/5ed91c84a1d0445020645add/watchlist`, {
+      method: "DELETE",
+      headers: {
+        "Authorization": "92cb03b6711b60551695b6ad33efd277918f03c4e92afdbf25b6df0297bcd34bb8f44c2c9e1ac8f39fef22af9d32b658b6a849990726b0b64090e241770a79f6b9e902dd987d9726488090e251f2d115979d01df574c1f6477668357255789ef56ea121727095e89f426c4b37d34cdfba91c9ff125ef72e94be8d29928470c9a"
+      },
+      body: {
+        "showId": showStringId
+      }
+    })
+      .then(res => res.json())
+      .then(json => {
+        //setList(json.watchlist)
+        //.then(watchlist => {
+        // setList(watchlist)
+        console.log(json)
+      })
+    console.log(showStringId)
+  }
 
   //let src = `https://image.tmdb.org/t/p/w342/${imageSrc}`
 
@@ -57,15 +79,14 @@ export const Profile = ({ loggedIn, setLoggedIn }) => {
 
           {list.map((item) => (
 
-            <section className="watch-list-container" key={item.id}>
+            <section className="watch-list-container" key={item.showId}>
               <div className="watch-item">
                 <div className="watch-list-left-container">
                   TITLE<p>{item.title}</p>
-                  <button className="remove-button" onClick={() => handleLogOut()}>REMOVE</button>
+                  <button className="remove-button" onClick={() => removeItem(item.showId)}>REMOVE</button>
                 </div>
                 <div className="movie-image">
                   <img
-
                     draggable={false}
                     alt={item.title}
                     style={{ width: "100%" }}
