05f689ccb300049cffbfa26f7f8ec3247dfd229d
Your Name <malin.marie@hotmail.se>
2020-06-18 12:57:10 +0200

    added remove

--- src/pages/Profile.js
+++ src/pages/Profile.js
@@ -9,14 +9,15 @@ export const Profile = ({ loggedIn, setLoggedIn }) => {
   const userId = useSelector((store) => store.user.login.userId)
   const [list, setList] = useState([])
 
+  const userID = "5ed91c84a1d0445020645add"
+
   useEffect(() => {
-    fetch("http://localhost:8080/users/5ed91c84a1d0445020645add/watchlist", {
+    fetch(`http://localhost:8080/users/${userID}/watchlist`, {
       method: "GET",
       headers: {
         "Authorization": "92cb03b6711b60551695b6ad33efd277918f03c4e92afdbf25b6df0297bcd34bb8f44c2c9e1ac8f39fef22af9d32b658b6a849990726b0b64090e241770a79f6b9e902dd987d9726488090e251f2d115979d01df574c1f6477668357255789ef56ea121727095e89f426c4b37d34cdfba91c9ff125ef72e94be8d29928470c9a"
       }
     })
-
       .then(res => res.json())
       .then(json => {
         setList(json.watchlist)
@@ -25,6 +26,25 @@ export const Profile = ({ loggedIn, setLoggedIn }) => {
         // console.log(watchlist)
       })
   }, [])
+
+  const removeItem = (showId) => {
+    fetch(`http://localhost:8080/users/${userID}/watchlist`, {
+      method: "DELETE",
+      headers: {
+        "Authorization": "92cb03b6711b60551695b6ad33efd277918f03c4e92afdbf25b6df0297bcd34bb8f44c2c9e1ac8f39fef22af9d32b658b6a849990726b0b64090e241770a79f6b9e902dd987d9726488090e251f2d115979d01df574c1f6477668357255789ef56ea121727095e89f426c4b37d34cdfba91c9ff125ef72e94be8d29928470c9a"
+      },
+      body: JSON.stringify ({
+        "showId": showId
+      })
+    })
+      .then(res => res.json())
+      .then(json => {
+        setList(json.watchlist)
+        //.then(watchlist => {
+        // setList(watchlist)
+        // console.log(watchlist)
+      })
+  }
 
   const capitalizeFirstLetter = (string) => {
     return string.charAt(0).toUpperCase() + string.slice(1);
@@ -55,8 +75,8 @@ export const Profile = ({ loggedIn, setLoggedIn }) => {
           {list.map((item) => (
             <section className="watch-list-container">
               <div className="watch-item">TITLE
-            <p className="watch-item-title">{item.title}</p>
-                <button className="remove-button" onClick={() => handleLogOut()}>REMOVE</button>
+              <p className="watch-item-title">{item.title}</p>
+                <button className="remove-button" onClick={() => removeItem(item.id)}>REMOVE</button>
 
               </div>
             </section>
