f303ff3018d0a5f94335e8c029fe13b05f8cc23f
Your Name <malin.marie@hotmail.se>
2020-06-20 12:58:25 +0200

    navlinks

--- src/components/Navbar/HamburgerMenu.js
+++ src/components/Navbar/HamburgerMenu.js
@@ -1,5 +1,5 @@
 import React, { useState, useEffect } from 'react'
-import { Link } from 'react-router-dom'
+import { NavLink  } from 'react-router-dom'
 import { RightArrow } from '../RightArrow'
 import { RandomMovieGenerator } from '../RandomMovieGenerator'
 
@@ -25,37 +25,37 @@ export const HamburgerMenu = ({ API_KEY }) => {
         <div className="hamburger-div-3"></div>
       </div>
       <ul className={`hamburger-menu ${show ? "visible" : ""}`}>
-        <Link to="/"> <li>Home</li></Link>
+        <NavLink exact to="/"> <li>Home</li></NavLink>
         <li className="genres" onClick={() => handleClick()}>Genres
           < RightArrow visible={visible} />
         </li>
         <div className={`hamburger-menu-genre-accordion ${visible ? "visible" : ""}`}>
-          <Link className="genre" to="/genres"><p>All Genres</p></Link>
-          <Link className="genre" to="/genres/Action/28"><p>Action</p></Link>
-          <Link className="genre" to="/genres/Adventure/12"><p>Adventure</p></Link>
-          <Link className="genre" to="/genres/Animation/16"><p>Animation</p></Link>
-          <Link className="genre" to="/genres/Comedy/35"><p>Comedy</p></Link>
-          <Link className="genre" to="/genres/Crime/80"><p>Crime</p></Link>
-          <Link className="genre" to="/genres/Documentary/99"><p>Documentary</p></Link>
-          <Link className="genre" to="/genres/Drama/18"><p>Drama</p></Link>
-          <Link className="genre" to="/genres/Family/10751"><p>Family</p></Link>
-          <Link className="genre" to="/genres/Fantasy/14"><p>Fantasy</p></Link>
-          <Link className="genre" to="/genres/History/36"><p>History</p></Link>
-          <Link className="genre" to="/genres/Horror/27"><p>Horror</p></Link>
-          <Link className="genre" to="/genres/Music/10402"><p>Music</p></Link>
-          <Link className="genre" to="/genres/Mystery/9648"><p>Mystery</p></Link>
-          <Link className="genre" to="/genres/Romance/10749"><p>Romance</p></Link>
-          <Link className="genre" to="/genres/Science%20Fiction/878"><p>Science Fiction</p></Link>
-          <Link className="genre" to="/genres/Thriller/53"><p>Thriller</p></Link>
-          <Link className="genre" to="/genres/TV%20Movie/10770"><p>TV Movie</p></Link>
-          <Link className="genre" to="/genres/War/10752"><p>War</p></Link>
-          <Link className="genre" to="/genres/Western/37"><p>Western</p></Link>
+          <NavLink className="genre" exact to="/genres"><p>All Genres</p></NavLink>
+          <NavLink className="genre" exact to="/genres/Action/28"><p>Action</p></NavLink>
+          <NavLink className="genre" exact to="/genres/Adventure/12"><p>Adventure</p></NavLink>
+          <NavLink className="genre" exact to="/genres/Animation/16"><p>Animation</p></NavLink>
+          <NavLink className="genre" exact to="/genres/Comedy/35"><p>Comedy</p></NavLink>
+          <NavLink className="genre" exact to="/genres/Crime/80"><p>Crime</p></NavLink>
+          <NavLink className="genre" exact to="/genres/Documentary/99"><p>Documentary</p></NavLink>
+          <NavLink className="genre" exact to="/genres/Drama/18"><p>Drama</p></NavLink>
+          <NavLink className="genre" exact to="/genres/Family/10751"><p>Family</p></NavLink>
+          <NavLink className="genre" exact to="/genres/Fantasy/14"><p>Fantasy</p></NavLink>
+          <NavLink className="genre" exact to="/genres/History/36"><p>History</p></NavLink>
+          <NavLink className="genre" exact to="/genres/Horror/27"><p>Horror</p></NavLink>
+          <NavLink className="genre" exact to="/genres/Music/10402"><p>Music</p></NavLink>
+          <NavLink className="genre" exact to="/genres/Mystery/9648"><p>Mystery</p></NavLink>
+          <NavLink className="genre" exact to="/genres/Romance/10749"><p>Romance</p></NavLink>
+          <NavLink className="genre" exact to="/genres/Science%20Fiction/878"><p>Science Fiction</p></NavLink>
+          <NavLink className="genre" exact to="/genres/Thriller/53"><p>Thriller</p></NavLink>
+          <NavLink className="genre" exact to="/genres/TV%20Movie/10770"><p>TV Movie</p></NavLink>
+          <NavLink className="genre" exact to="/genres/War/10752"><p>War</p></NavLink>
+          <NavLink className="genre" exact to="/genres/Western/37"><p>Western</p></NavLink>
         </div>
-        <Link to="/top-rated" ><li>Top rated</li></Link>
-        <Link to="/upcoming" ><li>Upcoming</li></Link>
-        <Link to="/trending-today" ><li>Trending today</li></Link>
-        <Link to="/trending-week"><li>Trending week</li></Link>
-        <Link to="/now-playing"><li>Now playing</li></Link>
+        <NavLink exact to="/top-rated" ><li>Top rated</li></NavLink>
+        <NavLink exact to="/upcoming" ><li>Upcoming</li></NavLink>
+        <NavLink exact to="/trending-today" ><li>Trending today</li></NavLink>
+        <NavLink exact to="/trending-week"><li>Trending week</li></NavLink>
+        <NavLink exact to="/now-playing"><li>Now playing</li></NavLink>
 
         < RandomMovieGenerator API_KEY={API_KEY} className="mobile-hamburger-menu" />
       </ul>
