6a90eef56aec796238634487c9c0070c01390c6a
Your Name <malin.marie@hotmail.se>
2020-06-22 15:56:17 +0200

    styling

--- src/components/MovieItem/IMDBRatingPlugin.js
+++ src/components/MovieItem/IMDBRatingPlugin.js
@@ -2,25 +2,8 @@ import React from 'react'
 
 export const IMDBRatingPlugin = ({ imdbId, title, rating }) => {
   return (
-    <div
-      class="imdbRatingPlugin"
-      data-title={imdbId}
-      data-style="p2"
-    >
-
-
-      {rating !== 0 &&
-        <p>
-          {rating}/10 ⭐
-        </p>
-      }
-
-    </div>
-  )
-}
-
-/*
-      <div className="imdb-logo">
+    <>
+      {/* <div className="imdb-logo">
         <a
           href={`https://www.imdb.com/title/${imdbId}/?ref_=plg_rt_1`}
           target="_blank"
@@ -31,5 +14,33 @@ export const IMDBRatingPlugin = ({ imdbId, title, rating }) => {
             src="https://ia.media-imdb.com/images/G/01/imdb/plugins/rating/images/imdb_38x18.png"
             alt={`${title} on IMDb`}
           />
+
         </a>
-      </div>*/
+      </div> */}
+    <div
+    class="imdbRatingPlugin"
+    data-title={imdbId}
+    data-style="p2"
+    >
+      <a
+      href={`https://www.imdb.com/title/${imdbId}/?ref_=plg_rt_1`}
+      target="_blank"
+      rel="noopener noreferrer"
+      >
+        <img
+        src="https://ia.media-imdb.com/images/G/01/imdb/plugins/rating/images/imdb_38x18.png"
+        alt={`${title} on IMDb`}
+        />
+      </a>
+
+      {rating !== 0 &&
+      <p>
+      {rating}/10 ⭐
+      </p>
+      }
+
+    </div>
+    </>
+  )
+}
+
