a8bc8c62b49c819409f255d078e58f6eb9225cce
jenniferot <56264114+Jenniferot@users.noreply.github.com>
2020-06-22 18:04:38 +0200

    moviesearchresults fixed

--- src/pages/MovieSearchResults.js
+++ src/pages/MovieSearchResults.js
@@ -23,11 +23,11 @@ export const MovieSearchResults = ({ API_KEY, loggedIn }) => {
   useEffect(() => {
     setLoading(true)
     fetch(URL)
-    .then(res => res.json())
-    .then(res => {
-      setMovies(res.results)
-      setLoading(false)
-    })
+      .then(res => res.json())
+      .then(res => {
+        setMovies(res.results)
+        setLoading(false)
+      })
   }, [URL])
 
   if (loading) {
@@ -50,15 +50,9 @@ export const MovieSearchResults = ({ API_KEY, loggedIn }) => {
             return (
               <div className="movie-wrapper" key={item.id}>
                 <Link className="movie-link" to={`/movie/${item.id}`}>
-<<<<<<< HEAD
-                  <img 
-                    className="movie-image"
-                    src={src}>
-=======
                   <img src={src}
                     className="movie-image"
                   >
->>>>>>> styling
                   </img>
                   <div className='movie-details'>
 
